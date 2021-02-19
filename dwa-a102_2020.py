07b7b1a95826dd6dcdaa28fba21b152ee9596a70
Edwin Echeverri <73259426+EdiSalazar@users.noreply.github.com>
2021-02-19 18:15:18 +0100

    Corrected
    Small improvements in some parameteres

--- dwa-a102_2020.py
+++ dwa-a102_2020.py
@@ -11,7 +11,9 @@ Following new DWA-102 (Dec. 2020)
 import numpy as np
 import pandas as pd
 import pyeto as pyeto
+from check_ranges import validRange
 from pyeto import thornthwaite, monthly_mean_daylight_hours, deg2rad
+
 
 #%% Berechnungsansatz B.3.1: Steildach (alle Deckungsmaterialien),
 #### Flachdach (Metall, Glas)  
@@ -22,15 +24,9 @@ def steep_roof(Area, P, ETp, Sp=0.3):
     between 0.1 and 0.6 mm. Standard storage value for steep roof
     in general is 0.3 mm, in case of glass or metal cover use 0.6 mm
     '''  
-    if (P < 500 or P > 1700):
-        return("Precipitation value is out of the range of validity"
-               " for the equation (500 - 1700 mm/a)")        
-    if (ETp < 450 or ETp > 700):
-        return("The value of potential evapotranspiration is out of the"
-               " range of validity for the equation (450 - 700 mm/a)")
-    if (Sp < 0.1 or Sp > 0.6):
-        return("The value of storage height (Sp) is out of the range "
-               "of validity for the equation (0.1 - 0.6 mm)")               
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+    validRange(Sp, 'Sp_steep_roof')  
     a = 0.9115 + 0.00007063*P - 0.000007498*ETp - 0.2063*np.log(Sp + 1)
     g = 0
     v = 1-a-g
@@ -80,15 +76,9 @@ def flat_roof(Area, P, ETp, Sp=1.0):
     Flat roof with asphalt or jointless concrete cover = 2.5;
     Flat roof with plaster (tight joints) cover = 1.5
     '''
-    if (P < 500 or P > 1700):
-        return("Precipitation value is out of the range of validity"
-               " for the equation (500 - 1700 mm/a)")        
-    if (ETp < 450 or ETp > 700):
-        return("The value of potential evapotranspiration is out of the"
-               " range of validity for the equation (450 - 700 mm/a)")
-    if (Sp < 0.6 or Sp > 3):
-        return("The value of storage height (Sp) is out of the range "
-               "of validity for the equation (0.6 - 3 mm)")
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+    validRange(Sp, 'Sp_flat_roof') 
     
     a = 0.8658 + 0.0001659*P - 0.00009945*ETp - 0.1542*np.log(Sp + 1)
     g = 0
@@ -118,7 +108,7 @@ def flat_roof(Area, P, ETp, Sp=1.0):
     results.Value = results.Value.round(3)
     return(results)
 # Test
-flat_roof(1000, 450, 450, 0.3)
+flat_roof(1000, 500, 450, 0.6)
 flat_roof(1000, 550, 450, 0.6)
 # sum(flat_roof(550, 450, 0.6))
 
@@ -133,26 +123,15 @@ def green_roof(Area, P, ETp, h, kf=70, WKmax_WP=0.5):
     (WKmax - WP) is 0.5
     '''
     
-    if (P < 500 or P > 1700):
-        return("Precipitation value is out of the range of validity"
-               " for the equation (500 - 1700 mm/a)")        
-    if (ETp < 450 or ETp > 700):
-        return("The value of potential evapotranspiration is out of the"
-               " range of validity for the equation (450 - 700 mm/a)")
-    if (h < 40 or h > 500):
-        return("Installation heigth (h) is out of the range "
-               "of validity for the equation (40 - 500 mm)")
-    if (kf < 18 or kf > 100):
-        return("Hydraulic conductivity (kf) is out of the range "
-               "of validity for the equation (18 - 100 mm/h)")
-    
-    if ((WKmax-WP) < 0.35 or (WKmax-WP) > 0.65):
-        return("Difference WKmax-WP is out of the range "
-               "of validity for the equation (0.35 - 0.65)")
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+    validRange(h, 'h_green_roof')
+    validRange(kf, 'kf_green_roof')
+    validRange(WKmax_WP, 'WKmax_WP_green_roof')
     
     a = (-2.182 + 0.4293*np.log(P) - 0.0001092*P + (236.1/ETp) + 0.0001142*h
-         + 0.0002297*kf + 0.01628*np.log(WKmax - WP)
-         - 0.1214*np.log((WKmax - WP)*h))
+         + 0.0002297*kf + 0.01628*np.log(WKmax_WP)
+         - 0.1214*np.log(WKmax_WP*h))
     g = 0
     v = 1-a-g
     RD = P*a
@@ -190,15 +169,9 @@ def storage_roof(Area, P, ETp, Sp=5):
     which should be bigger than 3 mm and lower than 10 mm.
     Standard value for Sp = 5.
     '''
-    if (P < 500 or P > 1700):
-        return("Precipitation value is out of the range of validity"
-               " for the equation (500 - 1700 mm/a)")        
-    if (ETp < 450 or ETp > 700):
-        return("The value of potential evapotranspiration is out of the"
-               " range of validity for the equation (450 - 700 mm/a)")
-    if (Sp < 3 or Sp > 10):
-        return("The value of storage height (Sp) is out of the range "
-               "of validity for the equation (3 - 10 mm)")
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+    validRange(Sp, 'Sp_storage_roof') 
     
     a = 0.9231 + 0.000254*P - 0.0003226*ETp - 0.1472*np.log(Sp+1)
     g = 0
@@ -233,7 +206,7 @@ storage_roof(Area=1000, P=550, ETp=450)
 #%% Berechnungsansatz B.3.5 & B.3.6: Teildurchlässige Flächenbeläge
 ### (Fugenanteil 2 % bis 10 %)
 # Partially permeable surfaces (Joint ratio 2 % to 10 %)
-def permeable_surface(Area, P, ETp, FA, kf, Sp=1, WKmax=0.35, WP=0.2):
+def permeable_surface(Area, P, ETp, FA, kf, Sp=1, WKmax_WP=0.15):
     '''Area corresponds to the surface of the element in m2, P stands
     for precipititaion (mm/a). ETp corresponds to potential
     evapotranspiration (mm/yr). FA corresponds to the joint ratio of
@@ -244,31 +217,19 @@ def permeable_surface(Area, P, ETp, FA, kf, Sp=1, WKmax=0.35, WP=0.2):
     hydraulic conductivity (mm/h), standard values are 18 mm/h,
     and 36 mm/h for FA = 4, and FA = 8, repectively.    
     '''
-    if (P < 500 or P > 1700):
-        return("Precipitation value is out of the range of validity"
-               " for the equation (500 - 1700 mm/a)")        
-    if (ETp < 450 or ETp > 700):
-        return("The value of potential evapotranspiration is out of the"
-               " range of validity for the equation (450 - 700 mm/a)")
-    if (FA < 2 or FA > 10):
-        return("The the joint ratio (FA) is out of the range "
-               "of validity for the equation (2 - 10 %)")
-    if (Sp < 0.1 or Sp > 2):
-        return("The value of storage height (Sp) is out of the range "
-               "of validity for the equation (0.1 - 2 mm)") 
-    if ((WKmax-WP) < 0.1 or (WKmax-WP) > 0.2):
-        return("Difference WKmax-WP is out of the range "
-               "of validity for the equation (0.1 - 0.2)")  
-    if (kf < 6 or kf > 100):
-        return("Hydraulic conductivity (kf) is out of the range "
-               "of validity for the equation (6 - 100 mm/h)")
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+    validRange(FA, 'FA_permeable_surface')
+    validRange(kf, 'kf_permeable_surface')
+    validRange(Sp, 'Sp_permeable_surface')
+    validRange(WKmax_WP, 'WKmax_WP_permeable_surface')
     
 ### Berechnungsansatz B.3.5: Teildurchlässige Flächenbeläge
 ### (Fugenanteil 2 % bis 5 %)
 # Partially permeable surfaces (Joint ratio 2 % to 5 %)   
     if (FA >= 2 and FA <= 5):
         a = (0.0800734*np.log(P) - 0.0582828*FA - 0.0501693*Sp
-             - 0.385767*(WKmax - WP) + (8.7040284/(11.9086896+kf)))
+             - 0.385767*WKmax_WP + (8.7040284/(11.9086896+kf)))
         # DWA-a-102 2020 equation:
         # g = (-0.2006 - 0.000253*ETp + 0.05615*FA - 0.0636*np.log(1 + Sp)
         #      + 0.1596*np.log(1 + kf) + 0.2778*(WKmax - WP))
@@ -283,7 +244,7 @@ def permeable_surface(Area, P, ETp, FA, kf, Sp=1, WKmax=0.35, WP=0.2):
 # This funtion can be joint with the previous one and add the param "slope"     
     if (FA >= 6 and FA <= 10):
         a = (0.05912*np.log(P) - 0.02749*FA - 0.03671*Sp
-             - 0.30514*(WKmax - WP) + (4.97687/(4.7975 + kf)))
+             - 0.30514*WKmax_WP + (4.97687/(4.7975 + kf)))
         # DWA-a-102 2020 equation:
         # g = (0.00004941*P - 0.0002817*ETp + 0.02566*FA - 0.03823*Sp
         #      + 0.691*np.exp(-6.465/kf))
@@ -317,7 +278,7 @@ def permeable_surface(Area, P, ETp, FA, kf, Sp=1, WKmax=0.35, WP=0.2):
     
 # Test
 permeable_surface(Area=1000, P=650, ETp=500, FA=8, kf=36)
-permeable_surface(1000, 650, 500, 4, 18)   
+permeable_surface(1000, 650, 500, 4, 18)  
 
 #%% Berechnungsansatz B.3.7: Teildurchlässige Flächenbeläge
 ### (Porensteine, Sickersteine), Kiesbelag, Schotterrasen
@@ -331,21 +292,11 @@ def porous_surface(Area, P, ETp, Sp=3.5, h=100, kf=180):
     conductivity (mm/h). Standard values are:
     Sp = 3.5 mm, h = 100 mm, kf = 180
     '''
-    if (P < 500 or P > 1700):
-        return("Precipitation value is out of the range of validity"
-               " for the equation (500 - 1700 mm/a)")        
-    if (ETp < 450 or ETp > 700):
-        return("The value of potential evapotranspiration is out of the"
-               " range of validity for the equation (450 - 700 mm/a)")
-    if (Sp < 2.5 or Sp > 4.2):
-        return("The value of storage height (Sp) is out of the range "
-               "of validity for the equation (2.5 - 4.2 mm)")
-    if (h < 50 or h > 100):
-        return("Installation heigth (h) is out of the range "
-               "of validity for the equation (50 - 100 mm)")
-    if (kf < 10 or kf > 180):
-        return("Hydraulic conductivity (kf) is out of the range "
-               "of validity for the equation (10 - 180 mm/h)")
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+    validRange(Sp, 'Sp_porous_surface')
+    validRange(h, 'h_porous_surface')
+    validRange(kf, 'kf_porous_surface')
 
     a = (0.000001969*P - 0.005116*np.log(Sp) - 0.0001051*h
          + 0.01753*np.exp(4.576/kf))
@@ -392,28 +343,18 @@ def paver_stonegrid(Area, P, ETp, FA=25, Sp=1, WKmax_WP=0.15):
     maximal water capacity (-) and WP to wilting point (-). Standard value
     for the difference (WKmax - WP) is 0.15.
     '''
-    if (P < 500 or P > 1700):
-        return("Precipitation value is out of the range of validity"
-               " for the equation (500 - 1700 mm/a)")        
-    if (ETp < 450 or ETp > 700):
-        return("The value of potential evapotranspiration is out of the"
-               " range of validity for the equation (450 - 700 mm/a)")
-    if (FA < 20 or FA > 30):
-        return("The the joint ratio (FA) is out of the range "
-               "of validity for the equation (20 - 30 %)")
-    if (Sp < 0.1 or Sp > 2):
-        return("The value of storage height (Sp) is out of the range "
-               "of validity for the equation (0.1 - 2 mm)") 
-    if ((WKmax-WP) < 0.1 or (WKmax-WP) > 0.2):
-        return("Difference WKmax-WP is out of the range "
-               "of validity for the equation (0.1 - 0.2)")  
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+    validRange(FA, 'FA_paver_stonegrid')
+    validRange(Sp, 'Sp_paver_stonegrid')
+    validRange(WKmax_WP, 'WKmax_WP_paver_stonegrid') 
 
     a = (0.145704 - 0.059177*np.log(FA) - 0.007354*Sp
-         - 0.050531*np.log(WKmax - WP))
+         - 0.050531*np.log(WKmax_WP))
     # g = (- 0.02927 + 0.1483*np.log(P) - 0.000269*ETp
     #      - 0.09913*np.log(1 + Sp) + 0.05222*(WKmax - WP))
     v = (1.106 - 0.1625*np.log(P) + 0.0001282*ETp
-         + 0.1131*np.log(1 + Sp) + 0.2848*(WKmax - WP))
+         + 0.1131*np.log(1 + Sp) + 0.2848*WKmax_WP)
     g = max((1 - (a + v)), 0.0)
     RD = P*a
     GWN = P*g
@@ -453,21 +394,11 @@ def gravel_cover(Area, P, ETp, h=100, Sp=3.5, kf=1.8):
     conductivity (mm/h). Standard values are:
     h = 100 mm, Sp = 3.5 mm, kf = 180
     '''
-    if (P < 500 or P > 1700):
-        return("Precipitation value is out of the range of validity"
-               " for the equation (500 - 1700 mm/a)")        
-    if (ETp < 450 or ETp > 700):
-        return("The value of potential evapotranspiration is out of the"
-               " range of validity for the equation (450 - 700 mm/a)")
-    if (h < 50 or h > 100):
-        return("Installation heigth (h) is out of the range "
-               "of validity for the equation (50 - 100 mm)")
-    if (Sp < 2.5 or Sp > 4.2):
-        return("The value of storage height (Sp) is out of the range "
-               "of validity for the equation (2.5 - 4.2 mm)")
-    if (kf < 0.72 or kf > 10):
-        return("Hydraulic conductivity (kf) is out of the range "
-               "of validity for the equation (0.72 - 10 mm/h)")      
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+    validRange(h, 'h_gravel_cover')
+    validRange(Sp, 'Sp_gravel_cover')
+    validRange(kf, 'kf_gravel_cover')     
     
     a = 0.00004517*P - 0.03454*np.log(Sp) + (0.1958/(0.2873 + kf))
     # g = (0.19761*np.log(P) - 0.000506*ETp + 0.016372*Sp - 0.001618*h
@@ -524,8 +455,10 @@ def drainage(Area, P, ETp, drainage_type):
     veg_drainage = ("Shallow ditches with vegetation",
                     "Ditch with vegetation", "ditch with vegetation",
                     "Flache Gräben mit Bewuchs", "Gräben mit Bewuchs")
-    #  The input could be change to a numeric value, like 1 = Rohr, Rinne..
-    #  and 2 = Flache Gräben mit Bewuchs.
+
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+
     if ((drainage_type in drainages) or
         (drainage_type in veg_drainage))  == False:
         return ("Wrong input as drinage-type")
@@ -575,15 +508,10 @@ def surf_infiltration(Area, P, ETp, kf, FAsf="FAsf_standard"):
     area. The standard value is calculated in terms of kf as:
     FAsf_standard = 94741*kf*exp(-1.195)
     '''
-    if (P < 500 or P > 1700):
-        return("Precipitation value is out of the range of validity"
-               " for the equation (500 - 1700 mm/a)")        
-    if (ETp < 450 or ETp > 700):
-        return("The value of potential evapotranspiration is out of the"
-               " range of validity for the equation (450 - 700 mm/a)")
-    if (kf < 325 or kf > 1100):
-        return("Hydraulic conductivity (kf) is out of the range "
-               "of validity for the equation (325 - 1100 mm/h)") 
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+    validRange(kf, 'kf_surf_infiltration') 
+    
     if (FAsf == "FAsf_standard"):
         FAsf = 94741*kf**(-1.195)
         
@@ -629,15 +557,10 @@ def infilt_swale(Area, P, ETp, kf, FAsm="FAsm_standard"):
     area (swale). The standard value is calculated in terms of kf as:
     FAsm_standard = 42.323*kf*exp(-0.314)
     '''
-    if (P < 500 or P > 1700):
-        return("Precipitation value is out of the range of validity"
-               " for the equation (500 - 1700 mm/a)")        
-    if (ETp < 450 or ETp > 700):
-        return("The value of potential evapotranspiration is out of the"
-               " range of validity for the equation (450 - 700 mm/a)")
-    if (kf < 14 or kf > 3600):
-        return("Hydraulic conductivity (kf) is out of the range "
-               "of validity for the equation (14 - 3600 mm/h)") 
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+    validRange(kf, 'kf_infilt_swale') 
+    
     if (FAsm == "FAsm_standard"):
         FAsm = 42.323*kf**(-0.314)       
     
@@ -684,15 +607,10 @@ def swale_trench(Area, P, ETp, kf, FAsm="FAsm_standard"):
     area (swale). The standard value is calculated in terms of kf as:
     FAsm_standard = 21.86*kf*exp(-0.348)
     '''
-    if (P < 500 or P > 1700):
-        return("Precipitation value is out of the range of validity"
-               " for the equation (500 - 1700 mm/a)")        
-    if (ETp < 450 or ETp > 700):
-        return("The value of potential evapotranspiration is out of the"
-               " range of validity for the equation (450 - 700 mm/a)")
-    if (kf < 3.6 or kf > 36):
-        return("Hydraulic conductivity (kf) is out of the range "
-               "of validity for the equation (3.6 - 36 mm/h)") 
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+    validRange(kf, 'kf_swale_trench')
+    
     if (FAsm == "FAsm_standard"):
         FAsm = 21.86*kf**(-0.348)
         
@@ -740,18 +658,12 @@ def swale_trench_system(Area, P, ETp, qDr, kf, FAsm="FAsm_standard"):
     terms of the hydraulic conductivity kf (mm/h) and qDr as:
     FAsm_standard = 11.79 - 3.14*LN(qDR) - 0.18594*kf
     '''
-    if (P < 500 or P > 1700):
-        return("Precipitation value is out of the range of validity"
-               " for the equation (500 - 1700 mm/a)")        
-    if (ETp < 450 or ETp > 700):
-        return("The value of potential evapotranspiration is out of the"
-               " range of validity for the equation (450 - 700 mm/a)")
-    if (qDr < 1 or qDr > 10):
-        return("Throttled discharge yield (qDr) is out of the range "
-               "of validity for the equation (1 - 10 l/(s*ha))")
-    if (kf < 0.36 or kf > 3.6):
-        return("Hydraulic conductivity (kf) is out of the range "
-               "of validity for the equation (3.6 - 36 mm/h)")    
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+    validRange(qDr, 'qDr_swale_trench_system')
+    validRange(kf, 'kf_swale_trench_system')
+    
+    
     if (FAsm == "FAsm_standard"):
         FAsm = 11.79 - 3.14*np.log(qDr) - 0.18594*kf
         
@@ -800,26 +712,14 @@ def rainwater_usage(P, ETp, VSp, VBr, FAbw=2, qBw=60):
     qBw stands for specific annual requirement for irrigation 
     (l/(m^2*year))
     '''
-    if (P < 500 or P > 1700):
-        return("Precipitation value is out of the range of validity"
-               " for the equation (500 - 1700 mm/a)")        
-    if (ETp < 450 or ETp > 700):
-        return("The value of potential evapotranspiration is out of the"
-               " range of validity for the equation (450 - 700 mm/a)")
-    if (VSp < 10 or VSp > 200):
-        return("The Specific storage (VSp) is out of the range "
-               "of validity for the equation (10 - 200 mm)")    
-    if (VBr < 0 or VBr > 5):
-        return("The available water volume (VBr) is out of the range "
-               "of validity for the equation (0 - 5 mm/d)")
-    if (FAbw < 0 or FAbw > 5):
-        return("The  proportion of irrigated area in relation to the "
-               " connected area is out of the range of validity for the"
-               " equation (0 - 5)")
-    if (qBw < 0 or qBw > 200):
-        return("The  Specific annual requirement for irrigation is out "
-               "of the range of validity for the equation "
-               "(0 - 200 l/(m^2*year)")    
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+    validRange(VSp, 'VSp_rainwater_usage')
+    validRange(VBr, 'VBr_rainwater_usage')
+    validRange(FAbw, 'FAbw_rainwater_usage')
+    validRange(qBw, 'qBw_rainwater_usage')
+    
+    
     VBw = FAbw*qBw
     Vnmin = min(P, 365*VBr + VBw)
     if VBw == 0:
@@ -864,24 +764,12 @@ def pod_system(P, ETp, Aw, A_1, a_1, A_2= 0, a_2= 0.0, A_3= 0, a_3= 0.0,
     pond (m2), a_i corresonds to proportion of area i (0.0-1.0),
     which directs its runoff to the pond (-)
     '''
-    if (P < 500 or P > 1700):
-        return("Precipitation value is out of the range of validity"
-               " for the equation (500 - 1700 mm/a)")        
-    if (ETp < 450 or ETp > 700):
-        return("The value of potential evapotranspiration is out of the"
-               " range of validity for the equation (450 - 700 mm/a)")
-    if (a_1 < 0.0 or a_1 > 1.0):
-        return("Proportion of area (a_1), which directs its runoff to " 
-               "the pond should be between 0.0 and 1.0")
-    if (a_2 < 0.0 or a_2 > 1.0):
-        return("Proportion of area (a_2), which directs its runoff to "
-               " the pond should be between 0.0 and 1.0")
-    if (a_3 < 0.0 or a_3 > 1.0):
-        return("Proportion of area (a_3), which directs its runoff to "
-               "the pond should be between 0.0 and 1.0")
-    if (a_4 < 0.0 or a_4 > 1.0):
-        return("Proportion of area (a_4), which directs its runoff to "
-               "the pond should be between 0.0 and 1.0")
+    validRange(P, 'P')
+    validRange(ETp, 'ETp')
+    validRange(a_1, 'a_1_pod_system')
+    validRange(a_2, 'a_2_pod_system')
+    validRange(a_3, 'a_1_pod_system')
+    validRange(a_4, 'a_1_pod_system')
     
     v = (ETp*Aw)/(P*(Aw + A_1*a_1 + A_2*a_2 + A_3*a_3 + A_4*a_4))
     a = 1 - v
